1f5deed4daa39223892da8c069654ec76f4dfaf6
Mateusz Korszun <mateusz.korszun@sumup.com>
2012-08-09 12:45:12 +0200

    delete players api + read attachment content

---
+++ apps/application_server_api/src/delete_player_api.erl
@@ -0,0 +1,70 @@
+%%% @author Mateusz Korszun <mkorszun@gmail.com>
+%%% @copyright (C) 2012, SaveCloud
+%%% @doc
+%%% Delete player API
+%%% @end
+%%% Created : 20 Jun 2012 by Mateusz Korszun <mkorszun@gmail.com>
+
+-module(delete_player_api).
+-export([out/1]).
+
+%% ###############################################################
+%% INCLUDES
+%% ###############################################################
+
+-include("api.hrl").
+
+%% ###############################################################
+%% CALLBACK FUNCTION
+%% ###############################################################
+
+out(A) ->
+    Args = yaws_api:parse_post(A),
+    {ok, DBName} = application:get_env(?APP, ?DB),
+    {ok, DB} = database:open(DBName),
+    Delete = fun() -> delete_user(DB, Args) end,
+    request:execute(validate(), Args, Delete).
+
+%% ###############################################################
+%% INTERNAL FUNCTIONS
+%% ############################################################### 
+
+delete_user(DB, Args) ->
+    case authorization:authorize(DB, Args) of
+        {ok, Result} ->
+            delete_user(DB, Args, Result);
+        {error, not_found} -> 
+            [{status, 404},{content, "text/xml", "User not found"}];
+        {error, _Error} ->
+            [{status, 500}, {content, "text/xml", "Internal error"}]
+    end.      
+
+delete_user(DB, Args, true) ->
+    User = proplists:get_value("user_id", Args),
+    case database:delete_doc(DB, User) of
+        {ok, _} ->
+            [{status, 200}, {content, "text/xml", "ok"}];
+        {error, empty} ->  
+            [{status, 400}, {content, "text/xml", "Save not found"}];
+        {error, _Reason} ->    
+            [{status, 500}, {content, "text/xml", "Internal error"}]
+    end;    
+
+delete_user(_, _, false) ->
+    [{status, 401}, {content, "text/xml", "Unauthorized"}].
+
+%% ############################################################### 
+%% VALIDATE PARAMS
+%% ############################################################### 
+
+validate() ->
+    [
+        {"user_id", undefined, 404, "text/xml", "Missing user name"},
+        {"user_id", [], 400, "text/xml", "Empty user name"},
+        {"password", undefined, 404, "text/xml", "Missing password"},
+        {"password", [], 400, "text/xml", "Empty password"}
+    ].
+
+%% ###############################################################
+%% ###############################################################
+%% ############################################################### 
---
+++ apps/application_server_utils/src/attachments.erl
@@ -0,0 +1,55 @@
+%%% @author Mateusz Korszun <mkorszun@gmail.com> 
+%%% @copyright (C) 2012, SaveCloud
+%%% @doc
+%%% Attachments
+%%% @end
+%%% Created : 20 Jun 2012 by Mateusz Korszun <mkorszun@gmail.com>
+
+-module(attachments).
+-export([get/3]).
+
+%% ###############################################################
+%% MACROS
+%% ###############################################################
+
+-define(ATTACHMENT, <<"_attachments">>).
+-define(EXCLUDE, [<<"revpos">>, <<"stub">>]).
+
+%% ###############################################################
+%%
+%% ###############################################################
+
+get(DB, Docs, Include) when is_list(Docs) ->
+    lists:map(fun(Doc) -> get(DB, Doc, Include) end, Docs);
+
+get(DB, {L} = Doc, Include) when is_list(L) ->
+    case document:read(?ATTACHMENT, Doc) of
+        {Attachments} ->
+            Id = document:get_id(Doc),
+            Fun = fun(A) -> attachments(DB, Id, A, Include) end,
+            New = lists:map(Fun, Attachments),
+            document:set_value(?ATTACHMENT, {New}, Doc);
+        undefined ->
+            Doc
+    end;
+get(_, Doc, _) ->
+    Doc.
+  
+%% ###############################################################
+%% INTERNAL FUNCTIONS
+%% ###############################################################
+  
+attachments(DB, Id, Attachment, Flag) when is_list(Flag) ->
+    attachments(DB, Id, Attachment, list_to_atom(Flag));
+attachments(DB, Id, Attachment, Flag) when is_binary(Flag) ->
+    attachments(DB, Id, Attachment, binary_to_list(Flag));
+attachments(_, _, {Name, Properties}, false) ->
+    {Name, document:delete(Properties, ?EXCLUDE)};
+attachments(DB, Id, {Name, Properties}, true) ->
+    {ok, Content} = couchbeam:fetch_attachment(DB, Id, Name),
+    Doc = document:delete(Properties, ?EXCLUDE),
+    {Name, document:set_value(<<"content">>, Content, Doc)}.
+
+%% ###############################################################
+%% ###############################################################
+%% ###############################################################
